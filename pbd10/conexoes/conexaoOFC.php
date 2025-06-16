dc9a64fface61d5882e9fc029753c3fa92bebe3b
Mariaduda92 <164566996+Mariaduda92@users.noreply.github.com>
2025-06-16 19:37:11 -0300

    Update conexaoOFC.php

--- pbd10/conexoes/conexaoOFC.php
+++ pbd10/conexoes/conexaoOFC.php
@@ -1,15 +1,11 @@
 <?php
-
-// Configurações do banco de dados
 $host = "localhost";
-$user = "usuario";
-$password = "senha";
-$dbname = "nome_do_banco";
+$user = "pdb_user";
+$password = "SenhaSegura123";
+$dbname = "pdb_db";
 
-// Criar conexão com o banco de dados
 $conn = new mysqli($host, $user, $password, $dbname);
 
-// Verificar conexão
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
