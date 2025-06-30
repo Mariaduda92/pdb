f87c99c92ded120cf524d7d6fa29075013724c63
Mariaduda92 <164566996+Mariaduda92@users.noreply.github.com>
2025-06-30 17:21:28 -0300

    Delete pbd10 directory

--- pbd10/dashboard_userOFC.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start(); //Faz a verificação 
-include 'conexoes/conexaoOFC.php';
-if (!isset($_SESSION['user_id'])) {
-    header("Location: ../Screens/Login/login.php");
-    exit();
-}
-
